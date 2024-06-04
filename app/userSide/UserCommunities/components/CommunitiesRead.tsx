e2509c6e2d102d42445f057fc4b88137b4a57c83
StunnaDawg <jonsonallen9@gmail.com>
2024-06-04 13:16:53 -0300

    unpin channels

--- app/userSide/UserCommunities/components/CommunitiesRead.tsx
+++ app/userSide/UserCommunities/components/CommunitiesRead.tsx
@@ -4,18 +4,19 @@ import {
   Pressable,
   ActivityIndicator,
   ScrollView,
+  Alert,
 } from "react-native"
-import React, { useEffect, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { useAuth } from "../../../supabaseFunctions/authcontext"
 import supabase from "../../../../lib/supabase"
 import { FileObject } from "@supabase/storage-js"
 import { Communities, CommunityChannel } from "../../../@types/supabaseTypes"
-import getSingleCommunity from "../../../supabaseFunctions/getFuncs/getSingleCommunity"
 import getUserPinnedChannels from "../../../supabaseFunctions/getFuncs/getUserPinnedChannels"
-import { useNavigation } from "@react-navigation/native"
+import { useFocusEffect, useNavigation } from "@react-navigation/native"
 import { NavigationType } from "../../../@types/navigation"
-import SinglePic from "../../../components/SinglePic"
 import SinglePicCommunity from "../../../components/SinglePicCommunity"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import showAlert from "../../../utilFunctions/showAlert"
 
 const CommunitiesRead = () => {
   const [loading, setLoading] = useState<boolean>(false)
@@ -26,6 +27,70 @@ const CommunitiesRead = () => {
   >([])
   const { user } = useAuth()
 
+  const showAlert = (onConfirm: () => void) =>
+    Alert.alert(
+      "Do you want to unpin this channel?",
+      "Please select an option.",
+      [
+        {
+          text: "Yes",
+          onPress: onConfirm,
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+      {
+        cancelable: true,
+      }
+    )
+
+  const removePinChannel = async (channelId: string) => {
+    showAlert(async () => {
+      try {
+        if (!user?.id) {
+          console.error("No user logged in!")
+          return
+        }
+
+        const { data: profile, error } = await supabase
+          .from("profiles")
+          .select("pinned_channels")
+          .eq("id", user.id)
+          .single()
+
+        if (error) {
+          console.error("Failed to fetch user profile:", error.message)
+          return
+        }
+
+        // filters out the pinned channel from the profile
+        const updatedPinnedChannels = profile.pinned_channels.filter(
+          (url: string) => url !== channelId
+        )
+
+        // Update the profile with the new array
+        const { error: updateError } = await supabase
+          .from("profiles")
+          .update({ pinned_channels: updatedPinnedChannels })
+          .eq("id", user.id)
+
+        if (updateError) {
+          console.error(
+            "Failed to update pinned channels:",
+            updateError.message
+          )
+        } else {
+          getUserPinnedChannels(setLoading, user?.id, setCommunityChannels)
+          console.log("Channel unpinned successfully!")
+        }
+      } catch (err) {
+        console.error("Error pinning channel:", err)
+      }
+    })
+  }
+
   useEffect(() => {
     if (!user) return
     getUserPinnedChannels(setLoading, user?.id, setCommunityChannels)
@@ -34,6 +99,23 @@ const CommunitiesRead = () => {
   useEffect(() => {
     console.log("community channels fetched", communityChannels)
   }, [communityChannels])
+
+  useFocusEffect(
+    useCallback(() => {
+      const getUser = async () => {
+        setLoading(true)
+        if (!user) return
+        getUserPinnedChannels(setLoading, user?.id, setCommunityChannels)
+        setLoading(false)
+      }
+
+      getUser()
+
+      return () => {
+        // Optional cleanup actions
+      }
+    }, [user, setCommunityChannels])
+  )
   return (
     <View className="mt-8 mx-8 pb-2 h-full">
       <View>
@@ -75,6 +157,14 @@ const CommunitiesRead = () => {
                     {c.recent_message || "No Messages yet!"}
                   </Text> */}
                 </View>
+              </Pressable>
+
+              <Pressable onPress={() => removePinChannel(c.id)}>
+                <MaterialCommunityIcons
+                  name="dots-vertical"
+                  size={24}
+                  color="white"
+                />
               </Pressable>
             </View>
           ))
