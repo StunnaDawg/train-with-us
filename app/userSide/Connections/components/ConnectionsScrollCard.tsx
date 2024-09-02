577dc42a49e62330ce244915d8927f77f8f5218c
StunnaDawg <jonsonallen9@gmail.com>
2024-09-02 16:00:20 -0300

    fixed styling entirely for now

--- app/userSide/Connections/components/ConnectionsScrollCard.tsx
+++ app/userSide/Connections/components/ConnectionsScrollCard.tsx
@@ -93,59 +93,47 @@ const ConnectionsScrollCard = ({
         imageStyle={styles.image} // Style for the image inside the ImageBackground
       >
         <View style={styles.overlay} />
-        {/* Optional overlay for darker background */}
 
-        <View className="flex flex-row items-center justify-center">
+        {/* Top Section */}
+        <View style={styles.topSection}>
           <Text className="font-bold text-2xl text-white">
             {profile.first_name}
           </Text>
           <Text className="font-bold text-3xl text-center mx-1 text-white">
             {calculateAge(profile.birthday)}
           </Text>
-          {profile.gender === "Male" ? (
-            <Foundation name="male-symbol" size={24} color="white" />
-          ) : profile.gender === "Female" ? (
-            <Foundation name="female-symbol" size={20} color="white" />
-          ) : (
-            <FontAwesome5 name="transgender-alt" size={20} color="white" />
-          )}
         </View>
 
-        <View className="flex flex-row justify-end items-center">
-          <View>
-            <MessageButton
-              setLoading={setLoading}
-              loading={loading}
-              profileId={profile?.id}
-              coach={false}
-              profilePic={profile?.profile_pic || ""}
-            />
-          </View>
+        {/* Middle Section */}
+        <View style={styles.middleSection}>
+          <MessageButton
+            setLoading={setLoading}
+            loading={loading}
+            profileId={profile?.id}
+            coach={false}
+            profilePic={profile?.profile_pic || ""}
+          />
         </View>
 
-        <View className="flex flex-row ">
-          <View>
-            <View className="flex flex-row items-center">
-              <FontAwesome6 name="person-running" size={20} color="white" />
-              <Text className="font-bold mx-1 text-white">
-                Fitness Interests
-              </Text>
-            </View>
-
-            <ScrollView
-              showsHorizontalScrollIndicator={false}
-              horizontal={true}
-              className="mt-1"
-            >
-              {profile?.activities && profile.activities.length > 0
-                ? profile.activities.map((tag) => (
-                    <View key={tag} className="mb-1">
-                      <ActivityTags activity={`${tag}`} />
-                    </View>
-                  ))
-                : null}
-            </ScrollView>
+        {/* Bottom Section */}
+        <View style={styles.bottomSection}>
+          <View className="m-2">
+            <Text className="text-white font-bold">{profile.about}</Text>
           </View>
+
+          <ScrollView
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            className="mt-1"
+          >
+            {profile?.activities && profile.activities.length > 0
+              ? profile.activities.map((tag) => (
+                  <View key={tag} className="mb-1">
+                    <ActivityTags activity={`${tag}`} />
+                  </View>
+                ))
+              : null}
+          </ScrollView>
         </View>
       </ImageBackground>
     </View>
@@ -155,17 +143,31 @@ const ConnectionsScrollCard = ({
 const styles = StyleSheet.create({
   imageBackground: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "space-between",
   },
   image: {
     borderRadius: 10,
     objectFit: "cover",
-    opacity: 0.8, // Adjust the opacity to make the content more readable
+    opacity: 0.8,
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Optional overlay to darken the background
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  topSection: {
+    marginHorizontal: 15,
+    marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  middleSection: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginRight: 20,
+  },
+  bottomSection: {
+    marginHorizontal: 15,
+    marginBottom: 100,
   },
 })
 
